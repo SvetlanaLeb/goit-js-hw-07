95bb902cd453870b585264fdffe7938aa0e2eb5d
Svetlana Lebedkova <lebedkova.svetl@yandex.ru>
2021-06-16 01:54:34 +0300

    Дополненное

--- js/task-08.js
+++ js/task-08.js
@@ -9,47 +9,56 @@ destroyButton.addEventListener('click', onButtonDestroy);
 
 
 let boxesArr = [];
+let lastElWidth=30;
+let lastElHeight=30;
 
-
-function createBoxes(amount) {
-  
-   for (let i = 0; i < amount; i += 1) {
+function creatingBoxes(amount) {
+   
+   const boxesArray = Array.from({ length: amount }, (v, i) => i + 1);
+   
+   const creatingBoxes = boxesArray.map((elem) => {
      
       const div = document.createElement('div');
-      
-      div.classList = 'box';
-      div.style.width = `${30 + i * 10}px`;
-      div.style.height = `${30 + i * 10}px`;
-      div.style.border=`${2 + i}px solid orangered`;
-      div.style.backgroundColor = `rgb(
-         ${Math.ceil(Math.random() * 250)}, 
-         ${Math.ceil(Math.random() * 250)}, 
-         ${Math.ceil(Math.random() * 250)}
-         )`;
-      
-      boxesArr.push(div)
-   }
    
-   boxes.append(...boxesArr)
+      div.classList = 'box';
+      div.style.width = `${lastElWidth + elem * 10}px`;
+      div.style.height = `${lastElHeight + elem * 10}px`;
+      div.style.border = `${2 + elem}px solid orangered`;
+      div.style.backgroundColor = `rgb(
+      ${Math.ceil(Math.random() * 250)}, 
+      ${Math.ceil(Math.random() * 250)}, 
+      ${Math.ceil(Math.random() * 250)}
+      )`;
+      boxesArr.push(div);
+      return div
+      
+   });
+   
+   lastElWidth = parseFloat(boxesArr[boxesArr.length - 1].style.width);
+   lastElHeight =parseFloat( boxesArr[boxesArr.length - 1].style.height);
+   
+   boxes.append(...creatingBoxes);
+   
 };
 
 function destroyBoxes(amount) {
    input.value = '';
-   
-   for (let i = 0; i < amount; i += 1) {
-      boxes.removeChild(boxesArr[i])
-   };
 
-   boxesArr=[]
+   boxesArr.forEach(elem=>boxes.removeChild(elem))
+
+   boxesArr = []
+   lastElWidth=30;
+   lastElHeight=30;
 };
 
 
 function onButtonRender(event) {
-   createBoxes(input.value);
+   creatingBoxes(input.value);
    
 };
 
 function onButtonDestroy(event) {
-  destroyBoxes(input.value)
+   destroyBoxes(input.value)
+   
 };
 
